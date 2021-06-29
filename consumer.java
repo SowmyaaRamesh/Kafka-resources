2a7820bb87207caae3db70946dc5136d0ecd3e91
Sowmya <sowmyaramesh4101@gmail.com>
2021-06-29 12:01:28 +0530

    Modified consumer code

--- consumer.java
+++ consumer.java
@@ -1,43 +1,54 @@
-import java.util.*;
-import org.apache.kafka.clients.consumer.ConsumerConfig;  
-import org.apache.kafka.clients.consumer.ConsumerRecord;  
-import org.apache.kafka.clients.consumer.ConsumerRecords;  
-import org.apache.kafka.clients.consumer.KafkaConsumer;  
-import org.apache.kafka.common.serialization.StringDeserializer;  
+package com.example.kafka;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import org.slf4j.Logger;  
-import org.slf4j.LoggerFactory;  
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
 
-import java.time.Duration;  
-import java.util.Arrays;  
-import java.util.Collections;  
-import java.util.Properties;  
+public class Consumer {
+    public static void main(String[] args) {
+        final Logger logger = LoggerFactory.getLogger(Producer.class);
 
-public class consumer{
-    public static void main(String[] args){
-         Logger logger= LoggerFactory.getLogger(consumer1.class.getName());  
-        String bootstrapServer = "127.0.0.1:9092";
-        String topic="TestTopic";
-        
-        Properties properties=new Properties();  
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapServer);  
-        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,   StringDeserializer.class.getName());  
-        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,StringDeserializer.class.getName());
+        // create variables for strings
+        final String bootstrapServers = "127.0.0.1:9092";
+        final String consumerGroupID = "java-group-consumer";
 
-        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"earliest");  
-        
-        //creating consumer  
-        KafkaConsumer<String,String> consumer= new KafkaConsumer<String,String>(properties);  
-        consumer.subscribe(Arrays.asList(topic));  
-                //polling  
-        while(true){  
-            ConsumerRecords<String,String> records=consumer.poll(Duration.ofMillis(100)); 
-            for(ConsumerRecord<String,String> record: records){  
-                logger.info("Key: "+ record.key() + ", Value:" +record.value());  
-                logger.info("Partition:" + record.partition()+",Offset:"+record.offset());  
-            }   
+        // Create properties object for Consumer
+        Properties prop = new Properties();
+        prop.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        prop.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        prop.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,StringDeserializer.class.getName());
+        prop.setProperty(ConsumerConfig.GROUP_ID_CONFIG,consumerGroupID);
+        //This might be a new group, has to consume for first time so specify offset to start consuming the records
+        prop.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"earliest");
 
+        //Create Consumer
+        final KafkaConsumer<String,String> consumer = new KafkaConsumer<>(prop);
+
+        //subscribe to topics
+        consumer.subscribe(Arrays.asList("TestTopic"));
+
+        //poll and consume the Records
+        while(true) {
+            ConsumerRecords<String,String> records = consumer.poll(Duration.ofMillis(1000));
+            for (ConsumerRecord record : records) {
+                logger.info("Received new record: \n"+
+                        "Key: "+ record.key()+", "+
+                        "Value: "+record.value()+", "+
+                        "Topic: "+record.topic()+", "+
+                        "Partition: "+record.partition()+", "+
+                        "Offset: "+record.offset()+"\n");
+            }
         }
+
     }
 }
